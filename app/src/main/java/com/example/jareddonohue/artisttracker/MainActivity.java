5df7a14509ce1e8c471c43d7ad0cba28c2d7335b
jdonohue44 <jared.donohue@gmail.com>
2016-11-22 13:15:11 -0500

    Comments and multiple artists

--- app/src/main/java/com/example/jareddonohue/artisttracker/MainActivity.java
+++ app/src/main/java/com/example/jareddonohue/artisttracker/MainActivity.java
@@ -4,26 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TabHost;
-import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private String COUNTRY = "us";
     private String LANGUAGE = "en";
-    private String QUERY = "Red Hot Chili Peppers";
-    private ArrayList<String> artistList = new ArrayList<>();
-    private String linkOfUrl = "";
-    public final static String URL_TO_LOAD = "";
+    private final static String pitchforkFeed   = "http://pitchfork.com/rss/news/";
+    private final static String rollingstoneFee = "http://www.rollingstone.com/music/rss";
+    private ArrayList<String> artistList;
+    public final static String URL_TO_LOAD = ""; // used for Intent
     private String finalUrl;
     private HandleXML xmlHandler;
     NewsItemAdapter adapter;
@@ -34,17 +27,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        grab the artists we want to fetch news on and
+        store them in ArrayList<String> artistList
+        */
+        loadTrackedArtists();
 
-        finalUrl = getSearchQuery(QUERY);
-        xmlHandler = new HandleXML(finalUrl, QUERY);
-        xmlHandler.fetchXML();
-        while(xmlHandler.parsingComplete);
-        listItems = xmlHandler.getNewsItems();
+        /*
+        fetch XML, parse it and store the NewsItems (Title, Link)
+        in ArrayList<NewsItem> listItems
+         */
+        fetchNewsItems(artistList);
 
+        /*
+        display the ListView of NewsItems using the NewsItemAdapter class
+         */
         initializeListView();
     }
 
-    private String getSearchQuery(String query) {
+    private void loadTrackedArtists(){
+        artistList = new ArrayList<String>();
+        artistList.add("The Growlers");
+        artistList.add("STRFKR");
+        artistList.add("Kanye West");
+        artistList.add("Funkadelic");
+        artistList.add("Led Zeppelin");
+        artistList.add("Red Hot Chili Peppers");
+    }
+
+    private void fetchNewsItems(ArrayList<String> artistList){
+        // first search http://pitchfork.com/rss/news/
+        // and http://www.rollingstone.com/music/rss for any matches
+        // to our tracked artists.
+
+        for(String artist : artistList){
+            finalUrl = getGoogleSearchQuery(artist);
+            xmlHandler = new HandleXML(finalUrl, artist);
+            xmlHandler.fetchXML();
+            while(xmlHandler.parsingComplete);
+            listItems.add(xmlHandler.getNewsItems().get(0));
+            listItems.add(xmlHandler.getNewsItems().get(1));
+        }
+
+    }
+
+    private String getGoogleSearchQuery(String query) {
         query = query.toLowerCase();
         String[] words = query.split(" ");
         String result = "";
